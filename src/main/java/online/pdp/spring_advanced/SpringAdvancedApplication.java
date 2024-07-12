020e8eb81352376953a1effe39fb16aadf156fca
AbdullohMaraimov <maraimovabdulloh.laptop2022@gmail.com>
2024-07-12 11:21:34 +0500

    class finished

--- src/main/java/online/pdp/spring_advanced/SpringAdvancedApplication.java
+++ src/main/java/online/pdp/spring_advanced/SpringAdvancedApplication.java
@@ -2,6 +2,7 @@ package online.pdp.spring_advanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.standard.ShellComponent;
 
 @SpringBootApplication
 public class SpringAdvancedApplication {
